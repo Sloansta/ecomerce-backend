35a80b00fac0cb0c8cde0a499667151ea5cddfd7
Sloan <Sloan.boyce@gmail.com>
2022-01-07 22:44:32 -0500

    modified some things in product-routes, added get routes for tag-routesa

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -135,7 +135,7 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
   Product.destroy({
     where: {
-      id: req.params.id
+      id: req.body.id
     }
   }).then(dbProductData => res.json(dbProductData))
     .catch(err => {
