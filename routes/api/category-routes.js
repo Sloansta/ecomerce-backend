12941aa8860bfdb0569c035688f5673c07cb6eab
Sloan <Sloan.boyce@gmail.com>
2022-01-08 22:33:52 -0500

    added put routes for category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -44,15 +44,58 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    id: req.body.id,
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
+// category update router
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    }
+  ).then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'No category with that id found' })
+      return
+    }
+    res.json(dbCategoryData)
+  })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbCategoryData => {
+    if(!dbCategoryData) {
+      res.status(404).json({ message: 'No category with that id found' })
+      return
+    }
+    res.json(dbCategoryData)
+  })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 });
 
 module.exports = router;
